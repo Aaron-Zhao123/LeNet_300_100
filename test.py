aecfe7af883c4f5e4a190ef7f8c444c6d34911a8
Zhao Aaron <Aaron@yaz21.corpus.private.cam.ac.uk>
2016-11-05 16:44:16 +0000

    implemented test_two

--- test.py
+++ test.py
@@ -62,7 +62,6 @@ def main():
     with tf.name_scope('cross_entropy'):
     # this cap is necessary to prevent 0s
         cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_fc, y))
-
         tf.scalar_summary('cross entropy', cross_entropy)
 
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
